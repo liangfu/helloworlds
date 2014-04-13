669e30cd9db2a3fca43240168545a50219842e17
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2014-04-13 15:27:57 +0800

    embedding tkinter;

---
+++ hellopy08-tkinter/main.cpp
@@ -0,0 +1,34 @@
+#include <Python.h>
+#include <pythonrun.h>
+
+int main(int argc, char *argv[])
+{
+  Py_NoSiteFlag=1;
+  Py_SetProgramName(argv[0]);
+  Py_SetPythonHome(".");
+  Py_InitializeEx(0);
+  PySys_SetArgv(argc, argv);
+  PyRun_SimpleString("import sys");
+  PyRun_SimpleString("sys.path = ['.','DLLs','python27.zip','python27.zip/Lib','python27.zip/Lib/lib-tk']");
+
+  PyRun_SimpleString("from time import time,ctime\n"
+    "import sys\n"
+    "import os\n"
+    "print sys.argv\n"
+    "print os.listdir('.')\n"
+    "print 'Today is',ctime(time())\n"
+    "print os.getcwd()\n"
+    "print sys.path\n"
+    //"import _tkinter;"
+    "import Tkinter;"
+    "from hello import Application\n"
+    "root = Tkinter.Tk()\n"
+    "app = Application(master=root)\n"
+    "app.mainloop()\n"
+    "root.destroy()\n"
+    );
+  //system("pause");
+  Py_Finalize();
+
+  return 0;
+}
